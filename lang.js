e665468c02a24f575b3db71530b2dccf87584417
isaacding <dingmocong2014@outlook.com>
2026-03-26 23:16:56 +0800

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,10 @@
+window.lang = {
+    "en-us": {
+        "title": "Homepage",
+        "text0": "Nothing here!"
+    },
+    "zh-cn": {
+        "title": "起始页",
+        "text0": "这里甚么也没有!"
+    }
+}
