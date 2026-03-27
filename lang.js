64ad9c19d3d646d8eade2b949bc75109f083ab43
isaacding <dingmocong2014@outlook.com>
2026-03-27 22:54:45 +0800

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,10 +1,14 @@
 window.lang = {
     "en-us": {
         "title": "Homepage",
-        "text0": "Nothing here!"
+        "text0": "Nothing here!",
+        "text1": "Other project",
+        "text2": "Class album"
     },
     "zh-cn": {
         "title": "起始页",
-        "text0": "这里甚么也没有!"
+        "text0": "这里甚么也没有!",
+        "text1": "其他项目",
+        "text2": "同学录"
     }
 }
